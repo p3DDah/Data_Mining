8a0bdba0a515fc5527d91a1768e40f28b99d5062
p3DDah <p3ddah-p-99@outlook.de>
2023-12-05 02:56:38 +0900

    first analysation

--- embedding.py
+++ embedding.py
@@ -6,6 +6,7 @@ import pandas as pd
 import logging
 
 from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 from sentence_transformers import SentenceTransformer
 from yellowbrick.cluster import KElbowVisualizer
 
@@ -21,28 +22,37 @@ def load_model():
     model = SentenceTransformer("all-MiniLM-L6-v2")
     return model
 
-def get_embedding(df, embeddings_path='embeddings.npy', mode="abstract"):
-    
+def get_embedding(df, embeddings_path='embeddings.npy', names_path='names.npy', mode="abstract"):
     model = load_model()
-    # df = get_data() already given in the main
-
-    # Check if the file exists
-    if not os.path.exists(embeddings_path):
-        # If the file does not exist, call the get_embedding method
-        logging.info("Creating CSV from JSON file.")
+    
+    # Check if the embeddings file exists
+    if not os.path.exists(embeddings_path) or not os.path.exists('abstract' + embeddings_path):
+        logging.info("Creating embeddings.")
+        
         if mode == "abstract":
+            # Generate embeddings and save them
             corpus_embeddings = model.encode(df["abstract"], show_progress_bar=True)
             np.save('abstract' + embeddings_path, corpus_embeddings, allow_pickle=True)
+            names = df["abstract"].tolist()
+            np.save('abstract' + names_path, names, allow_pickle=True)
         else:
             corpus_embeddings = model.encode(df["categories"], show_progress_bar=True)
             np.save(embeddings_path, corpus_embeddings, allow_pickle=True)
+            names = df["categories"].tolist()
+            np.save(names_path, names, allow_pickle=True)
+            
     else:
-        logging.info("The file already exists.")
-        corpus_embeddings = np.load(embeddings_path, allow_pickle=True)
-
-    logging.debug(corpus_embeddings.shape)
+        logging.info("The embeddings file already exists.")
+        if mode == "abstract":
+            corpus_embeddings = np.load('abstract' + embeddings_path, allow_pickle=True)
+            names = np.load('abstract' + names_path, allow_pickle=True)
+        else:
+            corpus_embeddings = np.load(embeddings_path, allow_pickle=True)
+            names = np.load(names_path, allow_pickle=True)
+            
+    logging.debug(corpus_embeddings.shape) # type: ignore
     
-    return corpus_embeddings
+    return corpus_embeddings, names
 
 def clustering(corpus_embeddings, num_clusters):
     
@@ -50,14 +60,13 @@ def clustering(corpus_embeddings, num_clusters):
     #clustering_model = KMeans(n_clusters=num_clusters)
     #clustering_model.fit(corpus_embeddings)
     #cluster_assignment = clustering_model.labels_
-
-    clustering_model = KMeans()
-    visualizer = KElbowVisualizer(clustering_model, k=(5,50))
-    visualizer.fit(corpus_embeddings)
-    visualizer.show
+    for i in range(10):
+        clustering_model = KMeans()
+        visualizer = KElbowVisualizer(clustering_model, k=(10+i*5, 15+i*5+1)) # type: ignore
+        visualizer.fit(corpus_embeddings)
+        visualizer.show(outpath=f"range_{10+i*5}_to_{15+i*5}.png")
 
     return #cluster_assignment
-
 
 def main():
     pass

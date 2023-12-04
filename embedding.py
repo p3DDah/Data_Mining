d7477820da0e861420dcbeccc900c3715678dd64
p3DDah <p3ddah-p-99@outlook.de>
2023-12-04 19:45:41 +0900

    elbow method

--- embedding.py
+++ embedding.py
@@ -7,6 +7,7 @@ import logging
 
 from sklearn.cluster import KMeans
 from sentence_transformers import SentenceTransformer
+from yellowbrick.cluster import KElbowVisualizer
 
 from preprocess import get_data
 
@@ -31,9 +32,10 @@ def get_embedding(df, embeddings_path='embeddings.npy', mode="abstract"):
         logging.info("Creating CSV from JSON file.")
         if mode == "abstract":
             corpus_embeddings = model.encode(df["abstract"], show_progress_bar=True)
+            np.save('abstract' + embeddings_path, corpus_embeddings, allow_pickle=True)
         else:
             corpus_embeddings = model.encode(df["categories"], show_progress_bar=True)
-        np.save(embeddings_path, corpus_embeddings, allow_pickle=True)
+            np.save(embeddings_path, corpus_embeddings, allow_pickle=True)
     else:
         logging.info("The file already exists.")
         corpus_embeddings = np.load(embeddings_path, allow_pickle=True)
@@ -43,13 +45,18 @@ def get_embedding(df, embeddings_path='embeddings.npy', mode="abstract"):
     return corpus_embeddings
 
 def clustering(corpus_embeddings, num_clusters):
-
+    
     logging.info("Start KMeans")
-    clustering_model = KMeans(n_clusters=num_clusters, verbose = True)
-    clustering_model.fit(corpus_embeddings)
-    cluster_assignment = clustering_model.labels_
+    #clustering_model = KMeans(n_clusters=num_clusters)
+    #clustering_model.fit(corpus_embeddings)
+    #cluster_assignment = clustering_model.labels_
 
-    return cluster_assignment
+    clustering_model = KMeans()
+    visualizer = KElbowVisualizer(clustering_model, k=(5,50))
+    visualizer.fit(corpus_embeddings)
+    visualizer.show
+
+    return #cluster_assignment
 
 
 def main():
